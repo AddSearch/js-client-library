0825ba240c750a0801b30e6c77e9eb04dc44885c
Antti Ala-Ilkka <antti@instereo.fi>
2020-08-12 13:34:11 +0300

    Add base64

--- src/util.js
+++ src/util.js
@@ -1,7 +1,18 @@
-var isFunction = function(fn) {
+const isFunction = function(fn) {
   return fn && {}.toString.call(fn) === '[object Function]';
 }
 
+const base64 = function(s) {
+  global.window = {};
+  if (window && window.btoa) {
+    return window.btoa(s);
+  }
+  else if (Buffer) {
+    return Buffer.from(s).toString('base64');
+  }
+}
+
 module.exports = {
-  isFunction: isFunction
+  isFunction,
+  base64
 }
