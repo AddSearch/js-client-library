6f864c192dcd929be90ae6fbc1df8067eb0b50be
Hao Phan <hao@addsearch.com>
2023-03-05 14:52:45 +0200

    unit tests

--- test/throttle.spec.js
+++ test/throttle.spec.js
@@ -1,49 +1,49 @@
-// import assert from "assert";
-// import throttle from "../src/throttle";
-//
-// function sleep(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-//
-// describe('throttle', function() {
-//
-//   it('function shouldn\'t be throttled (30 function calls in 600ms)', async function() {
-//     this.timeout(5000);
-//     var functionCalls = 30;
-//     var sleepTime = 20; // 30*20 = 600 ms
-//     var expectedExecutions = 30; // Function called in every iteration
-//     var executions = 0;
-//
-//     var f = function() {
-//       executions++;
-//     };
-//
-//     for (var i=0; i<functionCalls; i++) {
-//       f();
-//       await sleep(sleepTime);
-//     }
-//
-//     assert.equal(expectedExecutions, executions);
-//   });
-//
-//   it('function should be throttled (4 function calls in 600ms', async function() {
-//     this.timeout(5000);
-//     var functionCalls = 30;
-//     var sleepTime = 20; // 30*20 = 600 ms
-//     var throttleDelay = 200;
-//     var expectedExecutions = 3 + 1; // Call functions for 600 ms with 200 ms throttle = 3 calls + one last call
-//     var executions = 0;
-//
-//     var f = throttle(throttleDelay, function() {
-//       executions++;
-//     });
-//
-//     for (var i=0; i<functionCalls; i++) {
-//       f();
-//       await sleep(sleepTime);
-//     }
-//
-//     assert.equal(expectedExecutions, executions);
-//   });
-//
-// });
+import assert from "assert";
+import throttle from "../src/throttle.js";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('throttle', function() {
+
+  it('function shouldn\'t be throttled (30 function calls in 600ms)', async function() {
+    this.timeout(5000);
+    var functionCalls = 30;
+    var sleepTime = 20; // 30*20 = 600 ms
+    var expectedExecutions = 30; // Function called in every iteration
+    var executions = 0;
+
+    var f = function() {
+      executions++;
+    };
+
+    for (var i=0; i<functionCalls; i++) {
+      f();
+      await sleep(sleepTime);
+    }
+
+    assert.equal(expectedExecutions, executions);
+  });
+
+  it('function should be throttled (4 function calls in 600ms', async function() {
+    this.timeout(5000);
+    var functionCalls = 30;
+    var sleepTime = 20; // 30*20 = 600 ms
+    var throttleDelay = 200;
+    var expectedExecutions = 3 + 1; // Call functions for 600 ms with 200 ms throttle = 3 calls + one last call
+    var executions = 0;
+
+    var f = throttle(throttleDelay, function() {
+      executions++;
+    });
+
+    for (var i=0; i<functionCalls; i++) {
+      f();
+      await sleep(sleepTime);
+    }
+
+    assert.equal(expectedExecutions, executions);
+  });
+
+});
