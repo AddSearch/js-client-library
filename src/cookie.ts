98078bf6db3a1f04ae50fad62f55bfac0cb8b9d2
Hao Phan <hao@addsearch.com>
2025-03-10 15:12:09 +0200

    [sc-11776] use library "cookie" to handle cookie

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,37 +1,42 @@
+import cookie from 'cookie';
+
 const setCookie = (cookieName: string, cookieValue: string, expireDays: number): void => {
   if (typeof document === 'undefined') {
     return;
   }
+
   const date = new Date();
   date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
-  const expires = 'expires=' + date.toUTCString();
-  document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
+
+  document.cookie = cookie.serialize(cookieName, cookieValue, {
+    expires: date,
+    path: '/'
+  });
 };
 
 const getCookie = (cookieName: string): string | undefined => {
   if (typeof document === 'undefined') {
     return;
   }
-  const name = `${cookieName}=`;
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(name) === 0) {
-      return cookie.substring(name.length, cookie.length);
-    }
+
+  try {
+    const cookies = cookie.parse(document.cookie);
+    return cookies[cookieName];
+  } catch (error) {
+    console.debug('Error parsing cookies:', error);
+    return undefined;
   }
-  return undefined;
 };
 
 const deleteCookie = (name: string): void => {
   if (typeof document === 'undefined') {
     return;
   }
-  document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 2000 00:00:01 GMT;`;
+
+  document.cookie = cookie.serialize(name, '', {
+    expires: new Date(0),
+    path: '/'
+  });
 };
 
 export { setCookie, getCookie, deleteCookie };
